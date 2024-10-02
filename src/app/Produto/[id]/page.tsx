abb9cf22fa1d257313f2a7f2b9e7d0a18ff791d5
Julio Vasconcelos <julio_jpv@yahoo.com.br>
2024-10-02 04:36:50 -0300

    Alteração teste

--- src/app/Produto/[id]/page.tsx
+++ src/app/Produto/[id]/page.tsx
@@ -3,5 +3,5 @@ import produtos from "@/core/Constants/produtos"
 export default function PaginaProduto (props: any) {
     const id = +props.params.id
     const produto = produtos.find((p)=> p.id === id)
-    return produto ? <div>{produto?.nome}</div> : <div>Produto Não Encontrado</div>
+    return produto ? <div>{produto?.nome}</div> : <div>Produtos Não Encontrados</div>
 }
